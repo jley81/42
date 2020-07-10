8bdfbbf0e5a700ee55b27ec3c4f5a46f31552ae7
svost <ya.nowa@yandex.ru>
2020-07-10 23:19:24 +0300

    Update timestamps.h

--- src/main.h
+++ src/main.h
@@ -49,8 +49,6 @@ static const int64_t MIN_TXOUT_AMOUNT = MIN_TX_FEE;
 
 
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
-// Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
-static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 // Maximum number of script-checking threads allowed
 static const int MAX_SCRIPTCHECK_THREADS = 16;
 
