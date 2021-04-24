bd465b7f1b2919b1bda10b7fad5bb3f6d956d241
svost <ya.nowa@yandex.ru>
2021-04-24 23:57:28 +0300

    More less ifdef: success build with gcc, clang, icpc

--- src/serialize.h
+++ src/serialize.h
@@ -786,12 +786,10 @@ public:
         Init(nTypeIn, nVersionIn);
     }
 
-#if !defined(_MSC_VER) || _MSC_VER >= 1300
     CDataStream(const char* pbegin, const char* pend, int nTypeIn, int nVersionIn) : vch(pbegin, pend)
     {
         Init(nTypeIn, nVersionIn);
     }
-#endif
 
     CDataStream(const vector_type& vchIn, int nTypeIn, int nVersionIn) : vch(vchIn.begin(), vchIn.end())
     {
@@ -853,7 +851,6 @@ public:
     iterator insert(iterator it, const char& x=char()) { return vch.insert(it, x); }
     void insert(iterator it, size_type n, const char& x) { vch.insert(it, n, x); }
 
-#ifdef _MSC_VER
     void insert(iterator it, const_iterator first, const_iterator last)
     {
         assert(last - first >= 0);
@@ -866,7 +863,6 @@ public:
         else
             vch.insert(it, first, last);
     }
-#endif
 
 #ifndef _MSC_VER
     void insert(iterator it, std::vector<char>::const_iterator first, std::vector<char>::const_iterator last)
@@ -883,7 +879,6 @@ public:
     }
 #endif
 
-#if !defined(_MSC_VER) || _MSC_VER >= 1300
     void insert(iterator it, const char* first, const char* last)
     {
         assert(last - first >= 0);
@@ -896,7 +891,6 @@ public:
         else
             vch.insert(it, first, last);
     }
-#endif
 
     iterator erase(iterator it)
     {
